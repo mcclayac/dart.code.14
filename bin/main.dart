dc4cef095b32c9c7a06a9ba65ceb371678cf1ccd
mcclayac <anthony.mcclay@gmail.com>
2020-05-21 17:49:02 -0400

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,8 @@
+//code 1.14
+void main() {
+  String saySomething = "Hello John Smith";
+  var isFalse = true;
+  if(saySomething == null){
+    print("It is ${isFalse}");
+  }else print("It is not ${isFalse}");
+}
